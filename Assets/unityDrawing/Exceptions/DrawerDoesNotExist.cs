294d8afd003a5fe78adb0185856b3b64b75eadcb
Olly <43096197+Pineyless@users.noreply.github.com>
2026-06-15 22:27:16 +0200

    playground
    meh

---
+++ Assets/unityDrawing/Exceptions/DrawerDoesNotExist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DH.DrawingModule.Exceptions
+{
+    public class DrawerDoesNotExist : Exception
+    {
+        public DrawerDoesNotExist() : base("Drawer does not exist. Please create a drawer first")
+        {
+        }
+    }
+}
