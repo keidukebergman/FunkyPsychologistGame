ef7fb735131158bdc7f9a870c7041f6f91fec052
keidukebergman <54135223+keidukebergman@users.noreply.github.com>
2026-06-15 13:01:44 +0200

    Implement fatigue

---
+++ Assets/Scripts/Distraction/DistractionManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DistractionManager : MonoBehaviour
+{
+    public static DistractionManager instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
--- Assets/Scripts/Fatigue/FatigueManager.cs
+++ Assets/Scripts/Fatigue/FatigueManager.cs
@@ -2,25 +2,27 @@ using UnityEngine;
 
 public class FatigueManager : MonoBehaviour
 {
-    [SerializeField] private float fatigue = 0; //Fatigue as a percentage. Should never go beyond 100 nor below 0
-    [SerializeField] private float fatigueGrowthPerSecond = 2; //Fatigue increase as a percentage value per second
+    public static FatigueManager instance;
+    [SerializeField] private float fatigue = 0f; //Fatigue as a percentage. 
+    [SerializeField] private float fatigueGrowthPerSecond = 0.02f; //Fatigue increase as a percentage value per second
+    bool isDrainingFatigue = false; //Should probably only drain fatigue when talking to clients
 
     private void Start()
     {
-        
+        instance = this;
     }
 
     private void Update()
     {
-        if (fatigue < 100)
+        if (fatigue < 1.0f && isDrainingFatigue)
         {
             fatigue += fatigueGrowthPerSecond * Time.deltaTime;
-            if (fatigue > 100) fatigue = 100;
+            if (fatigue > 1.0f) fatigue = 1.0f;
         }
     }
 
 
-    //Getters, setters
+    //Getters, setters, modifiers
     public void SetFatigueGrowthPerSecond(float fatigueGrowthPerSecond)
     {
         this.fatigueGrowthPerSecond = fatigueGrowthPerSecond;
@@ -29,6 +31,21 @@ public class FatigueManager : MonoBehaviour
     public void SetFatigue(float fatigue)
     {
         this.fatigue = fatigue;
+    }
+
+    public void RemoveFatigue(float fatigue_value)
+    {
+        fatigue -= fatigue_value;
+    }
+
+    public void AddFatigue(float fatigue_value)
+    {
+        fatigue += fatigue_value;
+    }
+
+    public void SetFatigueDrain(bool should_drain)
+    {
+        isDrainingFatigue = should_drain;
     }
 
     public float GetFatigue()
