8c0a1fbc92ce5be22d1f4e7aadb5678b1078a28d
keidukebergman <54135223+keidukebergman@users.noreply.github.com>
2026-06-15 15:28:17 +0200

    Update DistractionManager.cs

--- Assets/Scripts/Distraction/DistractionManager.cs
+++ Assets/Scripts/Distraction/DistractionManager.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Runtime.CompilerServices;
 
 public class DistractionManager : MonoBehaviour
 {
-    public static DistractionManager instance;
+    [System.Serializable]
+    public class DistractionEntity
+    {
+        public DistractionEntity (float switchPenalty, float boredness, Interactable interactable)
+        {
+            this.switchPenalty = switchPenalty;
+            this.boredness = boredness;
+            this.interactable = interactable;
+        }
+        public float switchPenalty = 0;
+        public float boredness = 0;
+        public Interactable interactable;
+    }
 
+    public static DistractionManager instance;
+    [SerializeField] private Dictionary<Interactable, DistractionEntity> distractionEntities = new Dictionary<Interactable, DistractionEntity>();
+    private Interactable latest_interactable;
 
     private void Start()
     {
@@ -13,6 +31,14 @@ public class DistractionManager : MonoBehaviour
     private void Update()
     {
         
+    }
+
+    public void OnInteractWithInteractable(Interactable interactable, float focus_power)
+    {
+        if (interactable == null) return;
+        if (!distractionEntities.ContainsKey(interactable)) distractionEntities.Add(interactable, new DistractionEntity(0, 0, interactable));
+        DistractionEntity distractionEntity = distractionEntities[interactable];
+        if (interactable != latest_interactable) distractionEntity.switchPenalty = 1;
     }
 
 }
