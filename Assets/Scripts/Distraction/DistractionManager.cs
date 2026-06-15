d318f69e1d46ffdf30dcecdaaf1b343b0d1e3e9a
keidukebergman <54135223+keidukebergman@users.noreply.github.com>
2026-06-15 23:27:09 +0200

    Update distraction management

--- Assets/Scripts/Distraction/DistractionManager.cs
+++ Assets/Scripts/Distraction/DistractionManager.cs
@@ -23,9 +23,17 @@ public class DistractionManager : MonoBehaviour
     private FatigueManager fatigueManager;
     [SerializeField] private Dictionary<Interactable, DistractionEntity> distractionEntities = new Dictionary<Interactable, DistractionEntity>();
     private Interactable latestInteractable;
-    [SerializeField] private float borednessIncreasePerSecond = 0.1f;
-    [SerializeField] private float borednessDecreasePerActiveSecond = 0.3f;
+
+    [Header("Effect on active item per interaction")]
+    [SerializeField] private float borednessIncreasePerActiveSecond = 0.1f;
+    [SerializeField] private float borednessIncreasePerSingleInteraction = 0.2f;
+    [Space(10)]
+    [Header("Ambient effect")]
     [SerializeField] private float borednessDecreasePerAmbientSecond = 0.1f;
+    [Space(10)]
+    [Header("Effect on non-active items per interaction")]
+    [SerializeField] private float borednessDecreasePerSingleInteraction = 0.3f;
+    [SerializeField] private float borednessDecreasePerActiveSecond = 0.3f;
 
     private void Awake()
     {
@@ -55,7 +63,7 @@ public class DistractionManager : MonoBehaviour
         //Remove Fatigue. As boredness with the current item increases, the focus power of the item decreases.
         fatigueManager.RemoveFatigue(focus_power * (1 - distractionEntity.boredness));
         //Up that boredness, dude!
-        distractionEntity.boredness = Mathf.MoveTowards(distractionEntity.boredness, 1, borednessIncreasePerSecond * Time.deltaTime);
+        distractionEntity.boredness = Mathf.MoveTowards(distractionEntity.boredness, 1, borednessIncreasePerActiveSecond * Time.deltaTime);
         print(distractionEntity.boredness);
 
         //Remove boredness from other items
@@ -64,6 +72,35 @@ public class DistractionManager : MonoBehaviour
             if(key.Equals(interactable) == false)
             {
                 distractionEntities[key].boredness = Mathf.MoveTowards(distractionEntities[key].boredness, 0, borednessDecreasePerActiveSecond * Time.deltaTime); 
+            }
+        }
+        distractionEntities[interactable] = distractionEntity;
+    }
+
+    public void OnInteractWithSingleUseInteractable(Interactable interactable, float focus_power)
+    {
+        if (interactable == null) return;
+        //If we havent seen this interaction item before, add it to the list
+        if (!distractionEntities.ContainsKey(interactable)) distractionEntities.Add(interactable, new DistractionEntity(0, 0, interactable));
+        DistractionEntity distractionEntity = distractionEntities[interactable];
+
+        //If the interaction item is not the same as the previous, maybe we should penalize the player? Or something.
+        //if (interactable != latestInteractable) distractionEntity.switchPenalty = 1;
+
+        latestInteractable = interactable;
+
+        //Remove Fatigue. As boredness with the current item increases, the focus power of the item decreases.
+        fatigueManager.RemoveFatigue(focus_power * (1 - distractionEntity.boredness));
+        //Up that boredness, dude!
+        distractionEntity.boredness = Mathf.MoveTowards(distractionEntity.boredness, 1, borednessIncreasePerSingleInteraction);
+        print(distractionEntity.boredness);
+
+        //Remove boredness from other items
+        foreach (var key in distractionEntities.Keys)
+        {
+            if (key.Equals(interactable) == false)
+            {
+                distractionEntities[key].boredness = Mathf.MoveTowards(distractionEntities[key].boredness, 0, borednessDecreasePerSingleInteraction);
             }
         }
         distractionEntities[interactable] = distractionEntity;
